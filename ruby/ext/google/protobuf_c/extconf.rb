2aa68e1082c12fe1f885230b60267c4af3e48b77
Dave Benvenuti <davebenvenuti@gmail.com>
2025-03-20 22:17:21 +0000

    Rename debug_flags to additional_c_flags and tweak said flags

--- ruby/ext/google/protobuf_c/extconf.rb
+++ ruby/ext/google/protobuf_c/extconf.rb
@@ -32,12 +32,12 @@ debug_symbols_output_dir = determine_debug_symbols_output_dir(debug_enabled)
 ruby_major_minor = /(\d+\.\d+)/.match(RUBY_VERSION).to_s
 debug_symbols = "google-protobuf-#{Google::Protobuf::VERSION}-#{RUBY_PLATFORM}-ruby-#{ruby_major_minor}.dbg"
 
-debug_flags = debug_enabled ? "-O0" : "-O3 -DNDEBUG"
+additional_c_flags = debug_enabled ? "-O0 -fno-omit-frame-pointer -fvisibility=default" : "-O3 -DNDEBUG -fvisibility=hidden"
 
 if RUBY_PLATFORM =~ /darwin/ || RUBY_PLATFORM =~ /linux/ || RUBY_PLATFORM =~ /freebsd/
-  $CFLAGS += " -std=gnu99 #{debug_flags} -fvisibility=hidden -Wall -Wsign-compare -Wno-declaration-after-statement"
+  $CFLAGS += " -std=gnu99 -Wall -Wsign-compare -Wno-declaration-after-statement #{additional_c_flags}"
 else
-  $CFLAGS += " -std=gnu99 #{debug_flags}"
+  $CFLAGS += " -std=gnu99 #{additional_c_flags}"
 end
 
 if RUBY_PLATFORM =~ /linux/
