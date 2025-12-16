c5a145b7b9a5779972cd13838f958686e0daf65b
Kevin Menard <kevin.menard@shopify.com>
2025-12-16 17:05:00 -0500

    FIXME: More direct string encoding association.

--- ruby/ext/google/protobuf_c/convert.c
+++ ruby/ext/google/protobuf_c/convert.c
@@ -276,8 +276,7 @@ VALUE Convert_UpbToRuby(upb_MessageValue upb_val, TypeInfo type_info,
       }
       // UNSAFE: Using rb_str_new_static for benchmarking zero-copy performance.
       // This assumes the UPB arena outlives the Ruby string, which is not guaranteed.
-      VALUE str_rb = rb_str_new_static(upb_val.str_val.data, upb_val.str_val.size);
-      rb_enc_associate(str_rb, rb_utf8_encoding());
+      VALUE str_rb = rb_utf8_str_new_static(upb_val.str_val.data, upb_val.str_val.size);
       rb_obj_freeze(str_rb);
       return str_rb;
     }
@@ -295,7 +294,6 @@ VALUE Convert_UpbToRuby(upb_MessageValue upb_val, TypeInfo type_info,
       // UNSAFE: Using rb_str_new_static for benchmarking zero-copy performance.
       // This assumes the UPB arena outlives the Ruby string, which is not guaranteed.
       VALUE str_rb = rb_str_new_static(upb_val.str_val.data, upb_val.str_val.size);
-      rb_enc_associate(str_rb, rb_ascii8bit_encoding());
       rb_obj_freeze(str_rb);
       return str_rb;
     }
